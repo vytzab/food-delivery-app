3ee8129f526352c0fa4a19774d3aebe7e85f3a1b
vytzab <vytautas.zabielskas@gmail.com>
2024-05-02 17:41:38 +0300

    Implemented restaurant listing page

---
+++ src/app/restaurant-listing/restaurant-listing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RestaurantListingRoutingModule } from './restaurant-listing-routing.module';
+import { RestaurantListingComponent } from './components/restaurant-listing.component';
+
+
+@NgModule({
+  declarations: [
+    RestaurantListingComponent
+  ],
+  imports: [
+    CommonModule,
+    RestaurantListingRoutingModule
+  ],
+  exports: [RestaurantListingComponent]
+})
+export class RestaurantListingModule { }
